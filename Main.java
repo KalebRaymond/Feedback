11128a8a968449f40c9c8136ba092ec441519aca
Kaleb Raymond <43358126+KalebRaymond@users.noreply.github.com>
2018-10-13 14:17:37 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,9 +3,9 @@ package com.company;
 
 import javax.swing.*;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Scanner;
 
 public class Main {
-
     public static void main(final String... strings) throws InterruptedException,
             InvocationTargetException {
         SwingUtilities.invokeAndWait(new Runnable() {
@@ -17,7 +17,22 @@ public class Main {
                 } catch (Exception e) {
                     // ignore failure to set default look en feel;
                 }
-                JFrame frame = strings.length == 0 ? new Spectrogram(null) : new Spectrogram(strings[0]) ;
+                //JFrame frame = strings.length == 0 ? new Spectrogram(null) : new Spectrogram(strings[0]) ;
+                JFrame frame;
+                Scanner s = new Scanner(System.in);
+                System.out.println("(m)ic or (f)ile input?");
+                switch(s.nextLine().toLowerCase()) {
+                    case "f":
+                        System.out.println("Enter filepath of mp3: ");
+                        String filepath = s.nextLine();
+                        frame = new Spectrogram(filepath);
+                        break;
+                    case "m":
+                    default:
+                        frame = new Spectrogram(null);
+                        break;
+                }
+
                 frame.pack();
                 frame.setSize(640, 480);
                 frame.setVisible(true);
